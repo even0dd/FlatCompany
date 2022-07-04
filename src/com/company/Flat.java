de16a56f96516c763b4810ec558c7a308aea565f
brutender <fakedere@gmail.com>
2022-07-04 20:28:03 +0600

    Kyial and Kaliman

--- src/com/company/Flat.java
+++ src/com/company/Flat.java
@@ -5,17 +5,10 @@ public class Flat {
     private int floors;
     private int floor;
     private int price;
-    private String rayon;
+    private Area area;
     private String streat;
     private String houseNumber;
-
-    public String getRayon() {
-        return rayon;
-    }
-
-    public void setRayon(String rayon) {
-        this.rayon = rayon;
-    }
+    private Adres adres;
 
     public String getStreat() {
         return streat;
@@ -35,13 +28,11 @@ public class Flat {
 
     Flat(){};
 
-    public Flat(int numberOfRooms, int floors, int floor, int price, String rayon, String streat, String houseNumber) {
+    public Flat(int numberOfRooms, int floors, int floor, int price, String streat, String houseNumber) {
         this.numberOfRooms = numberOfRooms;
         this.floors = floors;
         this.floor = floor;
         this.price = price;
-
-        this.rayon = rayon;
         this.streat = streat;
         this.houseNumber = houseNumber;
     }
@@ -83,8 +74,75 @@ public class Flat {
                 + "\nЧисло комнат: " + numberOfRooms +"\nКоличество этажей: " + floors
                 + "\nЭтаж квартиры: " + floor
                 + "\nЦена: " + price
-                + "\nРайон: " + rayon
+                //+ "\nРайон: " + rayon
                 + "\nУлица: " + streat
                 + "\nНомер дома: " + houseNumber;
+    }
+    public enum Area {
+        PERVOMAEVSKIY("Первомайский район "),
+        OKTABRSKIY("Октябрьский район "),
+        LENINSKIY("Ленинский район "),
+        SVERDLOVSKIY("Свердловский район ");
+        Area(String s){
+            this.areaInRussia = s;
+        }
+        private String areaInRussia;
+        public String getAreaInRussia() {
+            return areaInRussia;
+        }
+    }
+
+    enum AdresPERVOMAEVSKIY {
+        ZhibekZholu("Жибек-Жолу"),
+        Abdrakhmanova("Советская"),
+        Akhunbaeva("Акхунбаева");
+        AdresPERVOMAEVSKIY(String s) {
+            this.adresInRussia = s;
+        }
+        private String adresInRussia;
+        public String getAdresInRussia() {
+            return adresInRussia;
+        }
+    }
+    enum AdresOKTABRSKIY{
+        BEBEEL("БИБИЛ"),
+        BOTKINA("Боткина"),
+        VOROVSKOY("Воровская");
+        AdresOKTABRSKIY(String s) {
+            this.adresInRussia = s;
+        }
+        private String adresInRussia;
+        public String getAdresInRussia() {
+            return adresInRussia;
+        }
+    }
+    enum AdresLENINSKIY{
+        Kazakova("Kaзакова"),
+        Lumumba("Лумумба"),
+        Kustanai("Кустанай");
+        AdresLENINSKIY(String s) {
+            this.adresInRussia = s;
+        }
+        private String adresInRussia;
+        public String getAdresInRussia() {
+            return adresInRussia;
+        }
+    }
+    enum AdresSVERDLOVSKIY{
+        AlexandraMatrosova("Александра Матросова"),
+        AnatolyGladkov("Анатолий Гладков"),
+        Queen("Королевская");
+        AdresSVERDLOVSKIY(String s) {
+            this.adresInRussia = s;
+        }
+        private String adresInRussia;
+        public String getAdresInRussia() {
+            return adresInRussia;
+        }
+    }
+    public static void newMethod(Area area) {
+        if(area.equals(Area.PERVOMAEVSKIY)) {
+            private AdresPERVOMAEVSKIY streat = AdresPERVOMAEVSKIY;
+        }
     }
 }
